3816e579fa0989c80d92074f208e92ec807d8c23
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-18 16:02:20 -0500

    Create route to return top 5 most purchased items

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,2 +1,14 @@
 class ReportsController < ApplicationController
+  def top_five_purchased_items
+    purchases = Order.joins(:item).order('count_all desc').limit(5).group(:item_id).count
+    if purchases.nil? || purchases.empty?
+      render json: { error_message: "No items found." }.to_json, status: 404
+    else
+      five_items = []
+      purchases.each do |key, value|
+        five_items.push(Item.find(key))
+      end
+      render json: five_items.to_json, status: 200
+    end
+  end
 end
