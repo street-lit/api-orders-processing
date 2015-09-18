f5aca88e38d4102fc9d2aff11f8acfa5df2cd968
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-17 21:23:41 -0500

    Add controllers for user, item, address and order models

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+
+  def index
+    users = User.all
+    render json: tasks.to_json, status: 200
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
