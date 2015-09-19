604cfd06cd2fbe463f5c7e4ea69fb3e26427d52d
MikeRobertHughes <hughes.mike.r@gmail.com>
2015-09-18 22:20:08 -0500

    Create route to return total amount of money earned in system

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,13 +2,22 @@ class ReportsController < ApplicationController
   def top_five_purchased_items
     purchases = Order.joins(:item).order('count_all desc').limit(5).group(:item_id).count
     if purchases.nil? || purchases.empty?
-      render json: { error_message: "No items found." }.to_json, status: 404
+      render json: { error_message: "Unable to find items." }.to_json, status: 404
     else
       five_items = []
       purchases.each do |key, value|
         five_items.push(Item.find(key))
       end
       render json: five_items.to_json, status: 200
+    end
+  end
+
+  def total_purchases
+    total = Order.joins(:item).sum("price * quantity")
+    if total.nil?
+      render json: { error_message: "Unable to find price." }.to_json, status: 404
+    else
+      render json: { total_purchases: total }.to_json, status: 200
     end
   end
 end
